4f4bb6d3d051ee77a42c2145fac3d81eff0d8099
Lee Cannon <leecannon@leecannon.xyz>
2023-04-02 00:48:00 +0100

    make tests actually run

--- build.zig
+++ build.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
-    var tests = b.addTest(.{
+    const tests = b.addTest(.{
         .root_source_file = .{ .path = "bitjuggle.zig" },
         .optimize = optimize,
     });
 
     const tests_step = b.step("test", "Run library tests");
-    tests_step.dependOn(&tests.step);
+    tests_step.dependOn(&tests.run().step);
 
     b.default_step = tests_step;
 }
