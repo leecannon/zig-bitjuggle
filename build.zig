9d8d52971ef8d1fac706ac13dcf6750143919a59
Lee Cannon <leecannon@leecannon.xyz>
2023-04-15 21:17:14 +0100

    update for zig master

--- build.zig
+++ build.zig
@@ -7,9 +7,10 @@ pub fn build(b: *std.Build) !void {
         .root_source_file = .{ .path = "bitjuggle.zig" },
         .optimize = optimize,
     });
+    const run_tests = b.addRunArtifact(tests);
 
     const tests_step = b.step("test", "Run library tests");
-    tests_step.dependOn(&tests.run().step);
+    tests_step.dependOn(&run_tests.step);
 
     b.default_step = tests_step;
 }
